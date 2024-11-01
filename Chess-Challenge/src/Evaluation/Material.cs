090cabd46ecd8680c51d2ff4f3c530ad94b3540b
William Fredlund <wfre@kth.se>
2024-11-01 14:44:08 +0100

    basic move generation

---
+++ Chess-Challenge/src/Evaluation/Material.cs
@@ -0,0 +1,25 @@
+﻿using ChessChallenge.API;
+
+namespace ChessChallenge.Evaluation
+{
+    public class Material
+    {
+        public const PieceType
+            PAWN = PieceType.Pawn,
+            KNIGHT = PieceType.Knight,
+            BISHOP = PieceType.Bishop,
+            ROOK = PieceType.Rook,
+            QUEEN = PieceType.Queen;
+
+        public static int MaterialValue(PieceType pieceType) => pieceType switch
+        {
+            PAWN => 100,
+            KNIGHT => 300,
+            BISHOP => 300,
+            ROOK => 500,
+            QUEEN => 900,
+            _ => 0,
+        };
+        
+    }
+}
