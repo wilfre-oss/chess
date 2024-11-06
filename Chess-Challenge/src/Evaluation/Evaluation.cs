23890a06d6e9a9a8dc0c0b906aef60e794790949
HenningWigforss <33979069+HenningWigforss@users.noreply.github.com>
2024-11-06 09:33:14 +0100

    1
    12

--- Chess-Challenge/src/Evaluation/Evaluation.cs
+++ Chess-Challenge/src/Evaluation/Evaluation.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ChessChallenge.Evaluation
 {
-    
+
     public class Evaluation
     {
         public const int PawnValue = 100;
@@ -18,7 +18,7 @@ namespace ChessChallenge.Evaluation
         public const int RookValue = 500;
         public const int QueenValue = 900;
 
-        public static int [] PieceSquarePawns = {
+        public static int[] PieceSquarePawnsWhite = {
                 0,  0,  0,  0,  0,  0,  0,  0,
                 50, 50, 50, 50, 50, 50, 50, 50,
                 10, 10, 20, 30, 30, 20, 10, 10,
@@ -26,10 +26,22 @@ namespace ChessChallenge.Evaluation
                 0,  0,  0, 20, 20,  0,  0,  0,
                 5, -5,-10,  0,  0,-10, -5,  5,
                 5, 10, 10,-20,-20, 10, 10,  5,
-                0,  0,  0,  0,  0,  0,  0,  0 
+                0,  0,  0,  0,  0,  0,  0,  0
         };
 
-        public static int[] PieceSquareKnights = {
+
+        public static int[] PieceSquarePawnsBlack = {
+    0,  0,  0,  0,  0,  0,  0,  0,
+    5, 10, 10,-20,-20, 10, 10,  5,
+    5, -5,-10,  0,  0,-10, -5,  5,
+    0,  0,  0, 20, 20,  0,  0,  0,
+    5,  5, 10, 25, 25, 10,  5,  5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    0,  0,  0,  0,  0,  0,  0,  0
+};
+
+        public static int[] PieceSquareKnightsWhite = {
             -50,-40,-30,-30,-30,-30,-40,-50,
             -40,-20,  0,  0,  0,  0,-20,-40,
             -30,  0, 10, 15, 15, 10,  0,-30,
@@ -40,7 +52,18 @@ namespace ChessChallenge.Evaluation
             -50,-40,-30,-30,-30,-30,-40,-50
         };
 
-        public static int[] PieceSquareBishops = {
+        public static int[] PieceSquareKnightsBlack = {
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50
+};
+
+        public static int[] PieceSquareBishopsWhite = {
             -20,-10,-10,-10,-10,-10,-10,-20,
             -10,  0,  0,  0,  0,  0,  0,-10,
             -10,  0,  5, 10, 10,  5,  0,-10,
@@ -51,7 +74,18 @@ namespace ChessChallenge.Evaluation
             -20,-10,-10,-10,-10,-10,-10,-20
         };
 
-        public static int[] PieceSquareRooks = {
+        public static int[] PieceSquareBishopsBlack = {
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20
+};
+
+        public static int[] PieceSquareRooksWhite = {
             0,  0,  0,  0,  0,  0,  0,  0,
             5, 10, 10, 10, 10, 10, 10,  5,
             -5,  0,  0,  0,  0,  0,  0, -5,
@@ -62,7 +96,19 @@ namespace ChessChallenge.Evaluation
             0,  0,  0,  5,  5,  0,  0,  0
         };
 
-        public static int[] PieceSquareQueen = {
+
+        public static int[] PieceSquareRooksBlack = {
+    0,  0,  0,  5,  5,  0,  0,  0,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+    5, 10, 10, 10, 10, 10, 10,  5,
+    0,  0,  0,  0,  0,  0,  0,  0
+};
+
+        public static int[] PieceSquareQueenWhite = {
             -20,-10,-10, -5, -5,-10,-10,-20,
             -10,  0,  0,  0,  0,  0,  0,-10,
             -10,  0,  5,  5,  5,  5,  0,-10,
@@ -72,8 +118,19 @@ namespace ChessChallenge.Evaluation
             -10,  0,  5,  0,  0,  0,  0,-10,
             -20,-10,-10, -5, -5,-10,-10,-20
         };
+        public static int[] PieceSquareQueenBlack = {
+   -20,-10,-10, -5, -5,-10,-10,-20,
+   -10,  0,  5,  0,  0,  0,  0,-10,
+   -10,  5,  5,  5,  5,  5,  0,-10,
+    0,  0,  5,  5,  5,  5,  0, -5,
+   -5,  0,  5,  5,  5,  5,  0, -5,
+   -10,  0,  5,  5,  5,  5,  0,-10,
+   -10,  0,  0,  0,  0,  0,  0,-10,
+   -20,-10,-10, -5, -5,-10,-10,-20
+};
 
-        public static int[] PieceSquareKingMid= {
+
+        public static int[] PieceSquareKingMidWhite = {
             -30,-40,-40,-50,-50,-40,-40,-30,
             -30,-40,-40,-50,-50,-40,-40,-30,
             -30,-40,-40,-50,-50,-40,-40,-30,
@@ -84,7 +141,18 @@ namespace ChessChallenge.Evaluation
             20, 30, 10,  0,  0, 10, 30, 20
         };
 
-        public static int[] PieceSquareKingEnd = {
+        public static int[] PieceSquareKingMidWBlack = {
+    20, 30, 10,  0,  0, 10, 30, 20,
+    20, 20,  0,  0,  0,  0, 20, 20,
+   -10,-20,-20,-20,-20,-20,-20,-10,
+   -20,-30,-30,-40,-40,-30,-30,-20,
+   -30,-40,-40,-50,-50,-40,-40,-30,
+   -30,-40,-40,-50,-50,-40,-40,-30,
+   -30,-40,-40,-50,-50,-40,-40,-30,
+   -30,-40,-40,-50,-50,-40,-40,-30
+};
+
+        public static int[] PieceSquareingKingEndWhite = {
             -50,-40,-30,-20,-20,-30,-40,-50,
             -30,-20,-10,  0,  0,-10,-20,-30,
             -30,-10, 20, 30, 30, 20,-10,-30,
@@ -95,9 +163,16 @@ namespace ChessChallenge.Evaluation
             -50,-30,-30,-30,-30,-30,-30,-50
         };
 
-
-
-
+        public static int[] PieceSquareingKingEndBlack = {
+   -50,-30,-30,-30,-30,-30,-30,-50,
+   -30,-30,  0,  0,  0,  0,-30,-30,
+   -30,-10, 20, 30, 30, 20,-10,-30,
+   -30,-10, 30, 40, 40, 30,-10,-30,
+   -30,-10, 30, 40, 40, 30,-10,-30,
+   -30,-10, 20, 30, 30, 20,-10,-30,
+   -30,-20,-10,  0,  0,-10,-20,-30,
+   -50,-40,-30,-20,-20,-30,-40,-50
+};
 
 
         public static int Evaluate(Board board)
@@ -126,10 +201,6 @@ namespace ChessChallenge.Evaluation
 
         static void CountPieceSquare(bool isWhite, Board board)
         {
-            //pawns
-            int pawns = 0;
-            Console.WriteLine(board.GetPieceBitboard(PieceType.Pawn, isWhite));
-
 
         }
     }
